5f05675a914e019e807c221c5c0f6c47643ba060
mudassiribrahim <mudassir.comsats2018@gmail.com>
2024-05-21 15:00:33 +0500

    create model

--- model.js
+++ model.js
@@ -1,4 +1,7 @@
+import blogSchema from './schema.js';
 /*
 to use our schema definition we need to convert our schema into a 
 model we can work with .to do so we can pass it into mongoose.model(modelName, schema) 
 */
+
+const blog = mongoose.model('Blog', blogSchema);
